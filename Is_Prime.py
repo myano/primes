00c606db1028a4803ce02fdcb4754e0ae0fd86bf
Michael Yanovich <michael@yanovich.net>
2011-12-28 18:20:52 -0500

    a little housekeeping

--- Is_Prime.py
+++ Is_Prime.py
@@ -3,40 +3,37 @@
 import math
 
 def Is_Prime(p):
-	''' 
-	This function determines if a given number 'p' is a prime number or not. 
-	'''
-	p=int(abs(p))
-	x=math.floor(math.sqrt(p))
-	a=3
-	z=x
-	if p == 0:
-		return False
-	elif p == 1:
-		return False
-	elif p == 2:
-		return True
-	elif p % 2 == 0:
-		return False
-	elif p >= 9:
-		if z % 2 == 0:
-			z+=1
-		while z >= 3:
-			if p % z == 0:
-				return False
-			if p % a == 0:
-				return False
-			z-=2
-			a+=2
-	return True
-			
+    '''
+    This function determines if a given number 'p' is a prime number or not.
+    '''
+    p = int(abs(p))
+    x = math.floor(math.sqrt(p))
+    a = 3
+    #z=x
+    if p == 0:
+        return False
+    elif p == 1:
+        return False
+    elif p == 2:
+        return True
+    elif p % 2 == 0:
+        return False
+    elif p >= 9:
+        if x % 2 == 0:
+            x += 1
+        while x >= 3:
+            if p % x == 0 or p % a == 0: return False
+            x -= 2
+            a += 2
+    return True
+
 if __name__ == '__main__':
-	print getattr(Is_Prime,'__doc__')
-	#num=raw_input("What number do you want to test? (To see if it is a prime?): ")
-	#num=1746860020068409
-	num=982451653
-	num=int(num)
-	if Is_Prime(num) == True:
-		print "The number, ", num, " is a prime number!"
-	else:	
-		print "The number, ", num, " is a composite number!"
+    #print getattr(Is_Prime,'__doc__')
+    #num=raw_input("What number do you want to test? (To see if it is a prime?): ")
+    num=1746860020068409
+    #num=982451653
+    num=int(num)
+    if Is_Prime(num) == True:
+        print "The number, ", num, " is a prime number!"
+    else:
+        print "The number, ", num, " is a composite number!"
